ee885b915352dfd017b69b5d619911bd380813f4
spiderzys <zouy34@wpa-6-967.cc.umanitoba.ca>
2016-04-07 01:06:20 -0500

    1

--- MyMovieApp/FirstViewController.m
+++ MyMovieApp/FirstViewController.m
@@ -79,7 +79,9 @@
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView{
     if (_result!=nil) {
+
         int currentPage = _moviePostImage.contentOffset.x/(_scrollHeight*2/3)+10;
+
         [_downLoadIndicator startAnimating];
         while (_movies.count<currentPage & _movies.count < _result.count) {
             scrollView.scrollEnabled = NO;
@@ -231,12 +233,7 @@
                                                                        ascending:NO];
         
         NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
-<<<<<<< HEAD
-
         temp = [temp subarrayWithRange:NSMakeRange(0, MIN(30, temp.count))];
-
-=======
->>>>>>> parent of aa0fceb... 1st
         temp = [temp sortedArrayUsingDescriptors:sortDescriptors];
         _result = [temp mutableCopy];
     }
